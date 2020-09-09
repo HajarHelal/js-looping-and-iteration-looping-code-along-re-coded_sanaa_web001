f1c8af5a38414603efa8bb96dd7511183751f36e
Hajar Helal <hajerhelal44@gmail.com>
2020-09-09 10:44:11 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -4,13 +4,15 @@ const arr= ["Lisa", "Kaitlin", "Jan"];
 const event=["surprise"];
 
 function writeCards(arr,event) {
+  let new=[];
   for (let i = 0; i < arr.length; i++){
     console.log(`Thank you ${arr[i]} for the wonderful ${event} gift!`);
+    new.push(`Thank you ${arr[i]} for the wonderful ${event} gift!`)
   }
-return arr,event;
+return new;
 }
 
-writeCards(arr,event);
+//writeCards(arr,event);
 
 
 
