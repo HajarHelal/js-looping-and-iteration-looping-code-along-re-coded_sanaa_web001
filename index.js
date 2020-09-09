02f30fda1145601e250775773cf70cff1e8d7a92
Hajar Helal <hajerhelal44@gmail.com>
2020-09-09 10:40:17 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -5,20 +5,20 @@ const event=["surprise"];
 
 function writeCards(arr,event) {
   for (let i = 0; i < arr.length; i++){
-    console.log(`Thank you ${arr[i]} for the wonderful ${event} gift!`);}
+    console.log(`Thank you ${arr[i]} for the wonderful ${event} gift!`);
+  }
 return arr,event;
 }
 
-writeCards(arr,event);
+//writeCards(arr,event);
 
 
 
-function writeCards(["Ada", "Brendan", "Ali"], "birthday"); {
-  for (let i = 0; i < writeCards().length; i++) {
-    console.log(`Thank you ${writeCards[i]} for the wonderful birthday gift!`);
-  }
-
-  return writeCards;
-}
-
-wrapGifts(gifts);
+// function wrapGifts(["Ada", "Brendan", "Ali"], "birthday"); {
+//   for (let i = 0; i < writeCards().length; i++) {
+//     console.log(`Thank you ${writeCards[i]} for the wonderful birthday gift!`);
+//   }
+// 
+//   return  ;
+// }
+// wrapGifts(gifts);
